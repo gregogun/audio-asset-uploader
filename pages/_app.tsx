b4258e8fed3ac686c005114dae60ac2da22b98e8
Greg Ogun <65421744+gregogun@users.noreply.github.com>
2023-07-15 21:50:47 +0100

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,32 @@
+import { globalCss } from "@/stitches.config";
+import type { AppProps } from "next/app";
+
+const globalStyles = globalCss({
+  "*, *::before, *::after": {
+    boxSizing: "inherit",
+  },
+  "*": {
+    "*:focus:not(.focus-visible)": {
+      outline: "none",
+    },
+  },
+  "html, body, #root, #__next": {
+    height: "100%",
+    fontFamily: "$body",
+    margin: 0,
+  },
+
+  "#__next": {
+    position: "relative",
+    zIndex: 0,
+  },
+  a: {
+    textDecoration: "none",
+  },
+});
+
+globalStyles();
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
